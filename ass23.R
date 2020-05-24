251750e08726c8c9bdbb2f570ed9fc379d46bd79
Mrunal Eknath Idhate <64097893+idhatemrunal@users.noreply.github.com>
2020-05-24 14:00:13 +0530

    Update ass23.R

--- ass23.R
+++ ass23.R
@@ -1,3 +1,5 @@
+#this fuction calculate the values of the mean functions 
+#set  value of the vectors
 my_vector<-function(a=numeric())
 {
   i<-NULL
@@ -7,17 +9,18 @@ my_vector<-function(a=numeric())
     i<<-NULL
     
   }
-  get<-function() a
-  setmean<-function(mean) i<<-mean
-  getmean<-function() m
+  get<-function() a #get the value of the vector
+  setmean<-function(mean) i<<-mean  #set the mean value of the vector
+  getmean<-function() m  #get the mean value of the vector
   list(set=set,get=get,setmean=setmean,getmean=getmean)
 }
 
 
-
+#The following function calculates the mean of the special “vector” created with the above function
+# first checks to see if the mean has already been calculated
 Mycache_mean<-function(a,...)
 {
-  i<-a$getmean()
+  i<-a$getmean()# it gets the mean from the cache and skips the computation
   if(is.null(i))
   {
     msg("getting cached data")
